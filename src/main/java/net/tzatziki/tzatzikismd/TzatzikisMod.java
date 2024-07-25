24fc8eda14e2d456d2011fa4e8476b0d214c1738
tzatziki1771 <thodorispsicho17@gmail.com>
2024-07-25 05:32:32 -0700

    Added Wild Garlic Item to the game (:

--- src/main/java/net/tzatziki/tzatzikismd/TzatzikisMod.java
+++ src/main/java/net/tzatziki/tzatzikismd/TzatzikisMod.java
@@ -2,6 +2,7 @@ package net.tzatziki.tzatzikismd;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tzatziki.tzatzikismd.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class TzatzikisMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
